0373ed1e5dd2f179060de3ab65daaf3b5177f0fa
SumitSohal07 <145088567+SumitSohal07@users.noreply.github.com>
2024-03-15 23:56:20 -0400

    Add files via upload

---
+++ product.java
@@ -0,0 +1,14 @@
+public class product {
+    public static int pro(int a, int b){
+        int product = a*b;
+        return product;
+    }
+    
+        public static void main(String args[]){
+            int a = 4;
+            int b = 5;
+            int prod = pro(a,b);
+            System.out.println("The product is:" + prod);
+        }
+    
+}
